967140dd034ba3db0ccaea27d12a054df3bb4495
Adrian Bradu <adrian.bradu@gmail.com>
2023-04-05 16:20:57 +0300

    removed overflow hidden from title and overview

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // material ui
-import { Stack, Box } from '@mui/material';
+import { Stack } from '@mui/material';
 import { styled } from '@mui/system';
 // components
 import { MemoizedHeading2 } from '../Typography/Heading2';
@@ -9,7 +9,6 @@ import { MemoizedBody } from '../Typography/Body';
 import { Caption } from '../Typography/Caption';
 // types
 import { MovieProps } from '../../types/types';
-import { StyledTitleBoxProps } from '../../types/types';
 
 const StyledStack = styled(Stack)(() => ({
   gap: '32px',
@@ -28,38 +27,13 @@ const HeaderBodyInfo = styled(Stack)(() => ({
   gap: '0px',
 }));
 
-const StyledTitleBox = styled(Box)<StyledTitleBoxProps>(({ theme }) => ({
-  overflow: 'auto',
-  whiteSpace: 'nowrap',
-  height: '48px',
-  [theme.breakpoints.up('lg')]: {
-    height: '56px',
-  },
-}));
-
-const StyledOverviewBox = styled(Box)(({ theme }) => ({
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  display: '-webkit-box',
-  WebkitLineClamp: 5,
-  WebkitBoxOrient: 'vertical',
-  height: '160px',
-  [theme.breakpoints.up('md')]: {
-    width: '60%',
-    WebkitLineClamp: 4,
-    height: '128px',
-  },
-}));
-
 export const Movie: React.FC<MovieProps> = ({ movies, activeSlide }) => {
   console.log('Movie Render');
 
   return (
     <StyledStack>
       <Header>
-        <StyledTitleBox id={'title'}>
-          <MemoizedHeading2 text={movies[activeSlide]?.title} />
-        </StyledTitleBox>
+        <MemoizedHeading2 text={movies[activeSlide]?.title} />
         <HeaderBody>
           <HeaderBodyInfo>
             <MemoizedHeading3 text={movies[activeSlide]?.release_date?.toString()} />
@@ -75,9 +49,7 @@ export const Movie: React.FC<MovieProps> = ({ movies, activeSlide }) => {
           </HeaderBodyInfo>
         </HeaderBody>
       </Header>
-      <StyledOverviewBox>
-        <MemoizedBody text={movies[activeSlide]?.overview} />
-      </StyledOverviewBox>
+      <MemoizedBody text={movies[activeSlide]?.overview} />
     </StyledStack>
   );
 };
